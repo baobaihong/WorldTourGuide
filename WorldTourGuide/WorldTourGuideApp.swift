a6f91f79aa5f9de841126a7522e8747c54278ae2
Jason Bao <baihongbao@outlook.com>
2024-02-10 11:26:23 +0800

    Initial Commit

---
+++ WorldTourGuide/WorldTourGuideApp.swift
@@ -0,0 +1,17 @@
+//
+//  WorldTourGuideApp.swift
+//  WorldTourGuide
+//
+//  Created by Jason on 2024/2/10.
+//
+
+import SwiftUI
+
+@main
+struct WorldTourGuideApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
